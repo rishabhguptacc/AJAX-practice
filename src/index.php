aba05011d0d4f1ff46e8d3a3ad75b4120aa9a311
Rishabh Gupta <rishabhgupta@cedcoss.com>
2022-03-15 16:18:52 +0530

    Initial commit to add all the files.

---
+++ src/index.php
@@ -0,0 +1,44 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>
+		Home
+	</title>
+	<link href="style.css" type="text/css" rel="stylesheet">
+</head>
+<body>
+	<div id="header">
+		
+		<nav>
+			<ul id="menu">
+				
+			</ul>
+		</nav>
+	</div>
+	<div id="main">
+		<div id="productForm">
+			<form action="index.php" method="get">
+				<label for="productId">ID <input type="text" name="productId" id="productId" > </label>
+				<label for="productName">NAME <input type="text" name="productName" id="productName" > </label>
+				<label for="productPrice">PRICE <input type="text" name="productPrice" id="productPrice" > </label>
+				
+				<p>
+					<input type="submit" name= "submit" id="submit" value="Submit">
+				</p>
+			</form>
+				
+			
+			
+		</div>
+
+
+	</div>
+	<div id="footer">
+		<nav>
+			<ul id="footer-links">
+				
+			</ul>
+		</nav>
+	</div>
+</body>
+</html>
